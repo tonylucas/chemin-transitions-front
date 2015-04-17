564eeff93ee8eb0cef5cbd20b2b7d15ec0667348
tonylucas <tony3lucas@hotmail.fr>
2015-04-17 15:54:51 +0200

    working filter

--- app/src/js/angular/app.js
+++ app/src/js/angular/app.js
@@ -15,99 +15,110 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise(function ($injector, $location) {
         var $state;
         $state = $injector.get("$state");
-        return $state.go("index");
+        return $state.go("index.structures");
     });
     return $stateProvider.state('index', {
-        url: "/map",
-        views: {
-            "": {
-                templateUrl: "partials/home.html",
-                controller: "HomeController"
+            views: {
+                "": {
+                    templateUrl: "partials/home.html",
+                    controller: "HomeController"
+                },
+                "navbar": {
+                    templateUrl: 'partials/navbar.html',
+                    controller: "NavBarController",
+                    "filter": {
+                        templateUrl: 'partials/ui/filter.html',
+                        controller: "HomeController"
+                    }
+                }
+
             },
-            "navbar": {
-                templateUrl: 'partials/navbar.html',
-                controller: "NavBarController",
+            onEnter: function (authService) {
+                if (authService.needsLogin) {
+                    return authService.showLogin();
+                }
+            }
+
+        })
+        .state('index.structures', {
+            views: {
+                "": {
+                    templateUrl: "partials/structures.html",
+                    controller: "StructuresController"
+                },
                 "filter": {
                     templateUrl: 'partials/ui/filter.html',
-                    controller: "HomeController"
+                    controller: "StructuresController"
                 }
             }
-
-        },
-        onEnter: function (authService) {
-            if (authService.needsLogin) {
-                return authService.showLogin();
-            }
-        }
-
-    }).state('index.actors', {
-        url: '/actors',
-        templateUrl: 'partials/structures/index.html'
-    }).state('users', {
-        url: "/users",
-        resolve: {
-            check: function ($state, authService) {
-                return authService.isAuthorize().then(function (data) {
-                    return authService.needsLogin = data;
-                });
-            }
-        },
-        views: {
-            "": {
-                template: "<div ui-view></div>"
+        }).state('index.actors', {
+            url: '/actors',
+            templateUrl: 'partials/structures/index.html'
+        }).state('users', {
+            url: "/users",
+            resolve: {
+                check: function ($state, authService) {
+                    return authService.isAuthorize().then(function (data) {
+                        return authService.needsLogin = data;
+                    });
+                }
             },
-            "navbar": {
-                templateUrl: 'partials/navbar.html',
-                controller: "NavBarController"
+            views: {
+                "": {
+                    template: "<div ui-view></div>"
+                },
+                "navbar": {
+                    templateUrl: 'partials/navbar.html',
+                    controller: "NavBarController"
+                }
             }
-        }
-    }).state('users.profile', {
-        url: "/profile",
-        controller: 'UsersController',
-        templateUrl: 'partials/users/profile.html'
-    }).state('index.invitation', {
-        url: "/invitation/:id",
-        controller: 'RegisterController',
-        onEnter: function (authService, userData, $stateParams, $state, ipCookie) {
-            if (ipCookie('token')) {
-                return $state.go('index');
-            }
-            userData.getInvitation($stateParams.id).then(function (invitation) {
-                if (!invitation || invitation.enable == false) {
+        }).state('users.profile', {
+            url: "/profile",
+            controller: 'UsersController',
+            templateUrl: 'partials/users/profile.html'
+        }).state('index.invitation', {
+            url: "/invitation/:id",
+            controller: 'RegisterController',
+            onEnter: function (authService, userData, $stateParams, $state, ipCookie) {
+                if (ipCookie('token')) {
                     return $state.go('index');
                 }
-                authService.setInvitation(invitation);
-                authService.showRegister();
-            })
-        }
-    }).state('structures', {
-        url: "/structures",
-        views: {
-            "": {
-                templateUrl: "partials/structures.html",
-                controller: "StructuresController"
+                userData.getInvitation($stateParams.id).then(function (invitation) {
+                    if (!invitation || invitation.enable == false) {
+                        return $state.go('index');
+                    }
+                    authService.setInvitation(invitation);
+                    authService.showRegister();
+                })
+            }
+        }).state('structures', {
+            url: "/structures",
+            views: {
+                "": {
+                    templateUrl: "partials/structures.html",
+                    controller: "StructuresController"
+                },
+                "navbar": {
+                    templateUrl: 'partials/navbar.html',
+                    controller: "NavBarController"
+                }
+            }
+        }).state('structures.show', {
+            url: '/:id',
+            templateUrl: "partials/structures/show.html",
+            resolve: {
+                id: function ($stateParams, Organisations) {
+                    return $stateParams.id;
+                }
             },
-            "navbar": {
-                templateUrl: 'partials/navbar.html',
-                controller: "NavBarController"
-            }
-        }
-    }).state('structures.show', {
-        url: '/:id',
-        templateUrl: "partials/structures/show.html",
-        resolve: {
-            id: function ($stateParams, Organisations) {
-                return $stateParams.id;
-            }
-        },
-        controller: "StructuresShowController"
-    }).state('skills', {
-        url: "/skills",
-        templateUrl: "partials/skills.html",
-        controller: "SkillsController"
-    }).state('assets', {
-        url: "/assets",
-        templateUrl: "partials/assets.html",
-        controller: "AssetsController"
-    });
+            controller: "StructuresShowController"
+        }).state('skills', {
+            url: "/skills",
+            templateUrl: "partials/skills.html",
+            controller: "SkillsController"
+        }).state('assets', {
+            url: "/assets",
+            templateUrl: "partials/assets.html",
+            controller: "AssetsController"
+        });
 });
--- app/src/js/angular/controllers/structuresController.js
+++ app/src/js/angular/controllers/structuresController.js
@@ -1,3 +1,63 @@
 app.controller('StructuresController', function ($scope, $stateParams, appConfig, mapService, Organisations, userData, skillData, $timeout, $filter) {
 
+
+    $scope.filteredStructures = [];
+    $scope.filteredActors = [];
+    $scope.filteredSkills = [];
+
+    $scope.orgs = Organisations.getOrganisations().then(function (orgs) {
+        angular.forEach(orgs, function (org, key) {
+
+            var coo = org.geometry.coordinates;
+            var city;
+
+            $.ajax({
+                url: 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + coo['1'] + ',' + coo['0'] + '&sensor=true',
+                success: function (data) {
+                    angular.forEach(data.results[0].address_components, function (comp) {
+                        if (comp.types[0] == 'locality') {
+                            city = comp.long_name;
+                        }
+                    });
+
+                    $scope.filteredStructures.push({
+                        name: org.properties.name,
+                        city: city,
+                        skills: org.properties.skills,
+                        id: org.id
+                    });
+                }
+            });
+
+        });
+    });
+
+    $scope.actors = userData.getPersons().then(function (persons) {
+        //        console.log(persons);
+        angular.forEach(persons, function (actor, key) {
+            $scope.filteredActors.push({
+                name: actor.fullName,
+
+            });
+        });
+
+    });
+
+    $scope.skills = skillData.getSkills().then(function (skills) {
+        //                console.log(skills);
+        angular.forEach(skills, function (skill, key) {
+            $scope.filteredSkills.push({
+                name: skill.name
+            });
+        });
+    });
+
+
+    $scope.filterSkills = function (skill) {
+        mapService.filterMarkers(skill.name);
+        mapService.fitMap();
+        $scope.searchText = skill.name;
+    }
+
+    
 });
