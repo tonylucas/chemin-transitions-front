93b489c504f0b573a9eac312d9dd97d71ace5150
benjamin thinkpad letmeknow <benjamin.gammaire@gmail.com>
2015-05-02 18:13:12 +0200

    update structure page template to fit techonmap datas

--- app/src/js/angular/controllers/structuresShowController.js
+++ app/src/js/angular/controllers/structuresShowController.js
@@ -1,16 +1,15 @@
 app.controller('StructuresShowController', function ($scope, $stateParams, Organisations, appConfig, authService) {
     $scope.domain = appConfig.domain();
     
-    Organisations.getOrganization($stateParams.id).then(function (data) {
+    Organisations.getOrganisation($stateParams.id).then(function (data) {
         console.log(data);
-
-    Organisations.getTechonmapDatas().then(function (techonmapdata) {
-
-        authService.getGeocode(data).then(function (response){
-            $scope.address = response[0].formatted_address;
-        });
-        var allData = _.union(data,techonmapdata);
-        $scope.org = allData;
-    });        
+        $scope.org = data; 
+    }, function(err) {
+        console.log(err);
+        Organisations.getTechonmapDatas().then(function (techonmapdata) {
+        var currentStructure = _.findWhere(techonmapdata, {id: $stateParams.id});
+        $scope.org = currentStructure;
+    });
     });
 });
+
